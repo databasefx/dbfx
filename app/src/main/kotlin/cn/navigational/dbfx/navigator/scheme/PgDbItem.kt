5a4dc5e7412a43d4673a92616a581d169ab5ff65
yangkui <752544765qq.com>
2020-10-08 22:33:53 +0800

    Optimize code structure

--- app/src/main/kotlin/cn/navigational/dbfx/navigator/scheme/PgDbItem.kt
+++ app/src/main/kotlin/cn/navigational/dbfx/navigator/scheme/PgDbItem.kt
@@ -2,9 +2,6 @@ package cn.navigational.dbfx.navigator.scheme
 
 import cn.navigational.dbfx.config.PG_DB_ICON
 import cn.navigational.dbfx.navigator.SchemeItem
-import cn.navigational.dbfx.navigator.folder.pg.PgExtensionFolder
-import cn.navigational.dbfx.navigator.folder.pg.PgAcMethodFolder
-import cn.navigational.dbfx.navigator.folder.pg.PgLanguageFolder
 import cn.navigational.dbfx.navigator.folder.pg.PgSchemeFolder
 
 class PgDbItem(private val dbName: String) : SchemeItem(PG_DB_ICON, dbName) {
@@ -17,15 +14,9 @@ class PgDbItem(private val dbName: String) : SchemeItem(PG_DB_ICON, dbName) {
         if (this.children.isNotEmpty()) {
             this.children.clear()
         }
-        val indexFolder = PgAcMethodFolder()
-        val langFolder = PgLanguageFolder()
         val schemeFolder = PgSchemeFolder(dbName)
-        val extensionFolder = PgExtensionFolder()
-        this.children.addAll(schemeFolder, indexFolder, extensionFolder, langFolder)
-        langFolder.initFolder()
-        indexFolder.initFolder()
+        this.children.add(schemeFolder)
         schemeFolder.initFolder()
-        extensionFolder.initFolder()
         loadStatusProperty().set(true)
     }
 }
