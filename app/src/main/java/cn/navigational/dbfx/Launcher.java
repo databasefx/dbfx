af913b14a482576f858f8f427f3be2f4dc2ae4ec
yangkui <752544765qq.com>
2020-10-08 22:31:26 +0800

    Fix javafx11+, runtime can't find subclass directly inheriting Application class.

--- app/src/main/java/cn/navigational/dbfx/Launcher.java
+++ app/src/main/java/cn/navigational/dbfx/Launcher.java
@@ -20,7 +20,7 @@ import java.net.ServerSocket;
  * @author yangkui
  * @since 1.0
  */
-public class Launcher extends Application {
+public class Launcher {
     /**
      * Application default occur port
      */
@@ -34,31 +34,12 @@ public class Launcher extends Application {
      * Global UI preference
      */
     public static UiPreferences uiPreference;
+    /**
+     * Socket must be a static variable. If it is a local variable,
+     * when the method is executed, the socket will be recycled with the death of the method stack.</p>
+     */
+    private static ServerSocket socket;
 
-    @Override
-    public void init() {
-        LOG.debug("Start init vertx options.");
-        var vertxOptions = new VertxOptions();
-        var fsOptions = new FileSystemOptions();
-        //Disable file cached
-        fsOptions.setFileCachingEnabled(false);
-        //Worker pool size
-        vertxOptions.setWorkerPoolSize(10);
-        vertxOptions.setFileSystemOptions(fsOptions);
-        VertxUtils.initVertx(vertxOptions);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        new SplashView();
-    }
-
-    @Override
-    public void stop() throws Exception {
-        LOG.debug("Stop current application.");
-        super.stop();
-        VertxUtils.close();
-    }
 
     /**
      * Determine whether the current application has been started
@@ -69,10 +50,10 @@ public class Launcher extends Application {
         LOG.debug("Start check current application whether startup.");
         var result = true;
         try {
-            new ServerSocket(PORT, 0, InetAddress.getByAddress(new byte[]{127, 0, 0, 1}));
+            socket = new ServerSocket(PORT, 0, InetAddress.getByAddress(new byte[]{127, 0, 0, 1}));
             result = false;
         } catch (Exception e) {
-            LOG.error("Current application instance already exist.", e);
+            LOG.info("Current application instance already exist.");
         }
         return result;
     }
@@ -81,6 +62,32 @@ public class Launcher extends Application {
         if (checkIfRunning()) {
             return;
         }
-        launch(args);
+        MainApp.launch(MainApp.class, args);
+    }
+
+    public static class MainApp extends Application {
+        @Override
+        public void init() {
+            LOG.debug("Start init vertx options.");
+            var vertxOptions = new VertxOptions();
+            var fsOptions = new FileSystemOptions();
+            //Disable file cached
+            fsOptions.setFileCachingEnabled(false);
+            //Worker pool size
+            vertxOptions.setWorkerPoolSize(10);
+            vertxOptions.setFileSystemOptions(fsOptions);
+            VertxUtils.initVertx(vertxOptions);
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            new SplashView();
+        }
+
+        @Override
+        public void stop() throws Exception {
+            LOG.debug("Stop current application.");
+            VertxUtils.close();
+        }
     }
 }
